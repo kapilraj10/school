b8543b1c1653850a87205f085d5c11e1808b5856
kapilraj10 <kapilrajkc10@gmail.com>
2026-04-07 13:29:12 +0545

    admin navabr fix

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -82,6 +82,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('Y.B.M')
+            ->brandLogo(asset('images/logo.png'))
+            ->brandLogoHeight('3rem')
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -92,6 +95,7 @@ class AdminPanelProvider extends PanelProvider
                 NavigationGroup::make()->label('Timetable Management'),
                 NavigationGroup::make()->label('View Timetable'),
                 NavigationGroup::make()->label('Timetable Settings'),
+                NavigationGroup::make()->label('Website Management'),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
